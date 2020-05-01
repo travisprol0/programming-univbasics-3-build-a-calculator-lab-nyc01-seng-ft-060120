70b82e2706427db0383b3575329847640d8f7cae
Travis Prol <travisjohnprol@gmail.com>
2020-05-01 21:16:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,7 +3,7 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  num2 - num1
+  num1 - num2
 end
 
 def division(num1, num2)
@@ -15,9 +15,9 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  num1 & num2
+  num1 % num2
 end
 
 def square_root(num)
-  
+
 end
